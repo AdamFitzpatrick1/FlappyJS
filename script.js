ae9eef6a813a837259fee9d1e2b925e0eee8f705
AdamFitzpatrick1 <31987448+AdamFitzpatrick1@users.noreply.github.com>
2021-01-21 09:40:47 +0000

    Refactored Code

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-
-const block = document.getElementById("block");
-const hole = document.getElementById("hole");
-const character = document.getElementById("character");
-const jumping = 0;
-const counter = 0;
+let block = document.getElementById("block");
+let hole = document.getElementById("hole");
+let character = document.getElementById("character");
+let jumping = 0;
+let counter = 0;
 
 hole.addEventListener('animationiteration', () => {
-    const random = -((Math.random()*300)+150);
+    let random = -((Math.random()*300)+150);
     hole.style.top = random + "px";
     counter++;
 });
 setInterval(function(){
-    const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     if(jumping==0){
         character.style.top = (characterTop+3)+"px";
     }
-    const blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    const holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
-    const cTop = -(500-characterTop);
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    let holeTop = parseInt(window.getComputedStyle(hole).getPropertyValue("top"));
+    let cTop = -(500-characterTop);
     if((characterTop>480)||((blockLeft<20)&&(blockLeft>-50)&&((cTop<holeTop)||(cTop>holeTop+130)))){
-        alert("Game over. Score: "+(counter));
+        alert("Game over. Score: "+(counter-1));
         character.style.top = 100 + "px";
         counter=0;
     }
 },10);
-counter = 0;
+
 function jump(){
     jumping = 1;
     let jumpCount = 0;
-    const jumpInterval = setInterval(function(){
-        const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let jumpInterval = setInterval(function(){
+        let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
         if((characterTop>6)&&(jumpCount<15)){
             character.style.top = (characterTop-5)+"px";
         }
